a3aefec9c442956257ba926e63542273f62d58eb
shashi-git-hub <shashishekharsharma563@gmail.com>
2025-06-10 22:34:18 +0530

    commit

--- oprator.java
+++ oprator.java
@@ -1,21 +1,60 @@
 public class oprator {
+
     public static void main(String args[]){
         // System.out.println((3>2) && (4<3));
         // System.out.println((3<6) || (3>2));
         // System.out.println((3>6) || (3<2));
-        System.out.println(!(3<6));
+        // System.out.println(!(3<6));
 
-        // int a = 5;   
-        // // a = a + 5;
-        // a += 5;  
+        // // int a = 5;   
+        // // // a = a + 5;
+        // // a += 5;  
 
-        int B = 10;
-        // B = B- 10;
-        B -= 10;
+        // int B = 10;
+        // // B = B- 10;
+        // B -= 10;
 
-        System.out.println(B);
+        // System.out.println(B);
+
+        class Tester {
+
+	
+		// Shift operator (<< and >>) is used to move the left operands value by
+		// the number of bits specified.
+		int a = 10;
+		int b = 20;
+		System.out.println(a << 2); // left-shift operator moves the value to
+									// the left side
+		System.out.println(b >> 3); // right-shift operator moves the value to
+									// the right side
+
+		// Unsigned right shift operator(>>>)
+		System.out.println(a >> 2);
+		System.out.println(a >>> 2);
+		// works in the same way for positive numbers
+
+		int c = -1;
+		System.out.println(c >> 2);
+		System.out.println(c >>> 2);
+
+		// There is no unsigned left shift operator(<<<). The below line leads
+		// to an error
+		// System.out.println(a<<<2);
+
+		// Bitwise operators are used to perform manipulation of individual bits
+
+		// Logical OR(||) does not check second condition if first is true
+		// Bitwise OR(|) always checks both conditions even if first condition
+		// is true or false
+		System.out.println(a | b);
+
+		// Logical AND(&&) does not check second condition if first is false
+		// Bitwise AND(&) checks both conditions even if first condition is true
+		// or false
+		// System.out.println(a & b);
+	
+
 
     }
 
 }
- 
